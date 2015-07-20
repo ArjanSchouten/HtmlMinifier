9951d944cc8ff40a39b099b234add95a1791cc53
Arjan Schouten <arjan-schouten@live.nl>
2015-07-20 15:43:45 +0200

    Added phpplaceholder

--- src/Placeholders/PHP/PHPPlaceholder.php
+++ src/Placeholders/PHP/PHPPlaceholder.php
@@ -11,12 +11,21 @@ class PHPPlaceholder implements PlaceholderInterface
     /**
      * Process the payload.
      *
-     * @param mixed $payload
-     *
+     * @param mixed $context
      * @return mixed
      */
     public function process($context)
     {
-        return $context;
+        $contents = $context->getContents();
+
+        $contents = preg_replace_callback('/<\?=((?!\?>).)*\?>/s', function ($match) use ($context) {
+            return $context->getPlaceholderContainer()->addPlaceholder($match[0]);
+        }, $contents);
+
+        $contents = preg_replace_callback('/<\?php((?!\?>).)*(\?>)?/s', function ($match) use ($context) {
+            return $context->getPlaceholderContainer()->addPlaceholder($match[0]);
+        }, $contents);
+
+        return $context->setContents($contents);
     }
 }
---
+++ tests/Placeholders/PHP/PHPPlaceholderTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use ArjanSchouten\HTMLMin\MinifyPipelineContext;
+use ArjanSchouten\HTMLMin\PlaceholderContainer;
+use ArjanSchouten\HTMLMin\Placeholders\PHP\PHPPlaceholder;
+use Mockery as m;
+
+class PHPPlaceholderTest extends PHPUnit_Framework_TestCase
+{
+
+    private $phpPlaceholder;
+
+    public function __construct()
+    {
+        $this->phpPlaceholder = new PHPPlaceholder();
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testPHPPlaceholder()
+    {
+        $placeholder = 'myPlaceholder';
+        $placeholderContainer = m::mock(PlaceholderContainer::class)->shouldReceive('addPlaceholder')->andReturn($placeholder)->getMock();
+        $context = new MinifyPipelineContext($placeholderContainer);
+
+        $result = $this->phpPlaceholder->process($context->setContents('<?php echo \''.PHP_EOL.'\';?>'));
+        $this->assertEquals($placeholder, $result->getContents());
+
+        $result = $this->phpPlaceholder->process($context->setContents('<?php echo \''.PHP_EOL.'\';?><?php echo \''.PHP_EOL.'\';?>'));
+        $this->assertEquals($placeholder.$placeholder, $result->getContents());
+
+        $result = $this->phpPlaceholder->process($context->setContents('<?php echo \''.PHP_EOL.'\';'));
+        $this->assertEquals($placeholder, $result->getContents());
+    }
+
+    public function testShortTags()
+    {
+        $placeholder = 'myPlaceholder';
+        $placeholderContainer = m::mock(PlaceholderContainer::class)->shouldReceive('addPlaceholder')->andReturn($placeholder)->getMock();
+        $context = new MinifyPipelineContext($placeholderContainer);
+
+        $result = $this->phpPlaceholder->process($context->setContents('<?= echo \'\';?>'));
+        $this->assertEquals($placeholder, $result->getContents());
+
+        $result = $this->phpPlaceholder->process($context->setContents('<p>This is a test</p><?= echo \''.PHP_EOL.'\';?><p>end</p>'));
+        $this->assertEquals('<p>This is a test</p>'.$placeholder.'<p>end</p>', $result->getContents());
+    }
+}

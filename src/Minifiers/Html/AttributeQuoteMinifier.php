c90ad06c20d20594948beca6a2599feac4b8a61a
Arjan Schouten <arjan-schouten@live.nl>
2015-08-19 22:12:47 +0200

    Updated quote minifier

--- src/Minifiers/Html/AttributeQuoteMinifier.php
+++ src/Minifiers/Html/AttributeQuoteMinifier.php
@@ -34,7 +34,19 @@ class AttributeQuoteMinifier implements MinifierInterface
      */
     public function process($context)
     {
-        $context->setContents(preg_replace_callback('/=\s*"([^\\\\"]*(?:\\\\"[^\\\\"]*)*)"/', function ($match) {
+        return $context->setContents(preg_replace_callback(
+            '/
+                =           # start matching by a equal sign
+                \s*         # its valid to use whitespaces after the equals sign
+                (["\'])?    # match a single or double quote and make it a capturing group for backreferencing
+                    (
+                            (?:(?=\1)|[^\\\\])*             # normal part of "unrolling the loop". Match no quote nor escaped char
+                            (?:\\\\\1                       # match the escaped quote
+                                (?:(?=\1)|[^\\\\])*         # normal part again
+                            )*                              # special part of "unrolling the loop"
+                    )       # use a the "unrolling the loop" technique to be able to skip escaped quotes like ="\""
+                \1?         # match the same quote symbol as matched before
+            /x', function ($match) {
             return $this->minifyAttribute($match);
         }, $context->getContents()));
 
@@ -52,13 +64,13 @@ class AttributeQuoteMinifier implements MinifierInterface
      */
     protected function minifyAttribute($match)
     {
-        if (Str::contains($match[1], $this->prohibitedChars)) {
+        if (Str::contains($match[2], $this->prohibitedChars)) {
             return $match[0];
-        } elseif (preg_match('/'.Constants::PLACEHOLDER_PATTERN.'/is', $match[1])) {
+        } elseif (preg_match('/'.Constants::PLACEHOLDER_PATTERN.'/is', $match[2])) {
             return $match[0];
         }
 
-        return '='.$match[1];
+        return '='.$match[2];
     }
 
     /**

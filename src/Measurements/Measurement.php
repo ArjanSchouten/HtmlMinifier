432fcd66980cc7fe505e4987319c0328681075f2
ArjanSchouten <mail@arjan-schouten.nl>
2015-09-11 21:11:59 +0200

    Added measurement class for future

---
+++ src/Measurements/Measurement.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ArjanSchouten\HtmlMinifier\Measurements;
+
+class Measurement
+{
+    /**
+     * @var array
+     */
+    private $steps;
+
+    /**
+     * Create a measurement with the input before modification.
+     *
+     * @param string $input
+     * @param string $keyName
+     */
+    public function __construct($input, $keyName = 'Initial')
+    {
+        $this->steps[$keyName] = mb_strlen($input, '8bit');
+    }
+
+    /**
+     * Add a step and measure the input size.
+     *
+     * @param string $input
+     * @param string $keyname
+     * @return array
+     */
+    public function addStep($input, $keyname = null)
+    {
+        if ($keyname == null) {
+            $keyname = 'Step: '.count($this->steps) + 1;
+        }
+
+        $this->steps[$keyname] = mb_strlen($input, '8bit');
+
+        return $this->steps;
+    }
+
+    /**
+     * Get all the steps which are measured.
+     *
+     * @return array
+     */
+    public function getSteps()
+    {
+        return $this->steps;
+    }
+}
